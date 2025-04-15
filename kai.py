7a615df5d61558994e1fa98e172e29187a228adb
DaltonDBug <105025649+DaltonDBug@users.noreply.github.com>
2025-04-14 21:32:38 -0400

    Update kai.py

--- kai.py
+++ kai.py
@@ -1,1 +1,9 @@
-print('Hello!')
+#game for KaiKai
+
+print("Hello there...")
+
+name = input(print("What is your name? ))
+
+print("Nice to meet you", name, "!")
+                   
+print("My name is Mr. Bean.")
